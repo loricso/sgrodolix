bb258c6cc61c823bb08346000c4809e8dd767973
loricso <loricso>
2025-01-13 00:17:23 +0100

    dai

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            "black": "#191414",
+            "white": "#fefefe",
+            "green": "#1DB954"
+        }
+    },
+    plugins: [],
+}
+
