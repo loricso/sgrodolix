7f0c258971836694d956bfbb80270bd7657287db
loricso <loricso>
2025-01-26 16:56:51 +0100

    0.4.1 daje

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-npx tailwindcss -o /home/lorix/projects/sgrodolix/src/css/build.css --minify
-zip release.zip /home/lorix/projects/sgrodolix/src/index.html /home/lorix/projects/sgrodolix/src/dai.html /home/lorix/projects/sgrodolix/release/dai.html /home/lorix/projects/sgrodolix/src/css/build.css
+npx tailwindcss -o /home/lorix/projects/sgrodolix/src/css/output.css --minify
+cd src
+zip -r ../release.zip ./
 
