84f747f064afd5638ae8d06c4b758133d6605ac9
loricso <ciminata08@gmail.com>
2025-11-29 16:58:16 +0100

    fixed design librix

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
- npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch
+ npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch & \
+ npx tailwindcss -i ./src/css/librix-in.css -o ./src/css/librix-out.css --watch
