0afd8551e606a87256a8493ab0b9d0d81613a903
PoulDev <filippotrabattoni@gmail.com>
2025-11-29 11:33:07 +0100

    build fix

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-npx tailwindcss -o /home/lorix/projects/sgrodolix/src/css/output.css --minify
+npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --minify
+npx tailwindcss -i ./src/css/librix-in.css -o ./src/css/librix-out.css --minify
 cd src
 zip -r ../release.zip ./
 
